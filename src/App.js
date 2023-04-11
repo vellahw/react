18a98f34a395c00e767cdbc5937afe2b802fb1cf
gksdnjs2@gmail.com <gksdnjs2@gmail.com>
2023-04-11 23:53:51 +0900

    첫 이벤트 처리 - 좋아요 누르면 숫자 증가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [title, updateTitle] = useState(['블로그 만들기', '리액트다아', '호롤롤롤']);
+  let [count, updateCount] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>My Blog</div>
       </div>
-      
+      <div className='list'>
+        <h3>{ title[0] } <span className='heart' onClick={()=>{ updateCount(count+1) }}>❤︎</span> {count} </h3> 
+        <p>홀홀홀...</p>
+        <hr/>
+        <h3>{ title[1] }</h3> 
+        <p>홀홀홀...</p>
+        <hr/>
+        <h3>{ title[2] }</h3> 
+        <p>홀홀홀...</p>
+        <hr/>
+      </div>
     </div>
   );
 }
