0e560142fab0db577860ae47471ab0787efdeb8a
gksdnjs2@gmail.com <gksdnjs2@gmail.com>
2023-04-12 23:29:38 +0900

    클릭 이벤트 구현

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [title, updateTitle] = useState(['블로그 만들기', '리액트다아', '호롤롤롤']);
-  let [count, updateCount] = useState(0);
+  let [titles, willUpdateTitle] = useState(['블로그 만들기', '리액트다아', '호롤롤롤']);
+  let [count, willUpdateCount] = useState(0);
 
+  function updateTitle(bulgogi) {
+    var newTitles = [...titles]; //Deep Copy
+    newTitles[0] = '불고기 만들기';
+    willUpdateTitle(newTitles);
+  }  
   return (
     <div className="App">
       <div className="black-nav">
         <div>My Blog</div>
       </div>
       <div className='list'>
-        <h3>{ title[0] } <span className='heart' onClick={()=>{ updateCount(count+1) }}>❤︎</span> {count} </h3> 
+        <h3>{ titles[0] } <span className='heart' onClick={()=>{ willUpdateCount(count+1) }}>❤︎</span> {count} </h3> 
         <p>홀홀홀...</p>
         <hr/>
-        <h3>{ title[1] }</h3> 
+        <h3>{ titles[1] }</h3> 
         <p>홀홀홀...</p>
         <hr/>
-        <h3>{ title[2] }</h3> 
+        <h3>{ titles[2] }</h3> 
         <p>홀홀홀...</p>
         <hr/>
       </div>
+      <button onClick={ updateTitle }>클릭하면 글 제목 변경</button>
     </div>
   );
 }
