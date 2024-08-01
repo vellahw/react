ddc5eb6d54d21c991a4c8dc5aec76c6b84d22568
vellahw <gksdnjs2@gmail.com>
2024-08-01 14:09:17 +0900

    make square component

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import Square from './components/Square';
 
 function App() {
   return (
-    <></>
+    <>
+      <div className='square-wrap'>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+        <Square></Square>
+      </div>
+    </>
   );
 }
 
--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Square(props) {
-  return (
-    <button className='square'>
-      X
-    </button>
-  );
+function Square() {
+  const [value, setValue] = useState(null);
+
+  const handleClick = () => {
+    setValue('X');
+  }
+  
+  return <button className="square" onClick={handleClick}>{value}</button>;
 }
 
 export default Square;
