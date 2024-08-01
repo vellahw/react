cfb3df286132bfcfc9d9879a7068ec118f2fd55c
vellahw <gksdnjs2@gmail.com>
2024-08-01 14:27:25 +0900

    define Square component

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,19 +1,30 @@
+import React, { useState } from 'react';
+
 import './App.css';
 import Square from './components/Square';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
+
+
   return (
     <>
-      <div className='square-wrap'>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
-        <Square></Square>
+      <div className="square-wrap">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
       </div>
     </>
   );
--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square({value, onSquareClick}) {
+  // const [value, setValue] = useState(null);
 
-  const handleClick = () => {
-    setValue('X');
-  }
+  // const handleClick = () => {
+  //   setValue('X');
+  // }
   
-  return <button className="square" onClick={handleClick}>{value}</button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default Square;
