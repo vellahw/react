7f4dfb8d9af59bb05b40fb2f10a586c7778da1a0
Vella <112395421+vellahw@users.noreply.github.com>
2026-05-21 01:20:03 +0900

    상태 업데이트

--- sucoding/src/App.tsx
+++ sucoding/src/App.tsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
 
 export default function App() {
+  const [state, setState] = useState<number>(0);
+  const handleClick = () => {
+    setState(10);
+  };
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
+  const handleIncrement = () => {
     // 상태 업데이트 함수
     // 이전 값을 참조해서 값을 변경
     setCount((count) => count + 1);
     setCount((count) => count + 1);
     setCount((count) => count + 1);
   };
+
   return (
     <>
+      <div>
+        <h1>Ep.05 useState</h1>
+        <hr />
+        <div>
+          <p>state: {state}</p>
+          <button type="button" onClick={handleClick}>
+            Set to 10
+          </button>
+        </div>
+      </div>
       <h1>Ep.05 함수형 업데이트 방식</h1>
       <hr />
       <div>
         <p>count: {count}</p>
       </div>
-
-      <button onClick={handleClick}>Update Profile</button>
+      <button onClick={handleIncrement}>Update Profile</button>
     </>
   );
 }
--- sucoding/src/EP05/EP05.tsx
+++ sucoding/src/EP05/EP05.tsx
@@ -5,17 +5,34 @@ export default function EP05() {
   const handleClick = () => {
     setState(10);
   };
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    // 상태 업데이트 함수
+    // 이전 값을 참조해서 값을 변경
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
 
   return (
     <>
-      <h1>useState</h1>
+      <div>
+        <h1>Ep.05 useState</h1>
+        <hr />
+        <div>
+          <p>state: {state}</p>
+          <button type="button" onClick={handleClick}>
+            Set to 10
+          </button>
+        </div>
+      </div>
+      <h1>Ep.05 함수형 업데이트 방식</h1>
       <hr />
       <div>
-        <p>state: {state}</p>
-        <button type="button" onClick={handleClick}>
-          Set to 10
-        </button>
+        <p>count: {count}</p>
       </div>
+      <button onClick={handleIncrement}>Update Profile</button>
     </>
   );
 }
